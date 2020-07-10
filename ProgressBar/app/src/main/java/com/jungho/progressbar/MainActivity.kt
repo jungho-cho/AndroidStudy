18bc44c3979f8e53b4a6f808eae33c7f3e67fe8b
jungho-cho <myjung2620@gmail.com>
2020-07-10 17:25:54 +0200

    Chip, ProgressBar example

---
+++ ProgressBar/app/src/main/java/com/jungho/progressbar/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.jungho.progressbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            textView.text = "현재 값 > ${progressBar4.progress}"
+        }
+
+        button2.setOnClickListener {
+            progressBar4.progress = 140
+        }
+
+        button3.setOnClickListener {
+            progressBar4.incrementProgressBy(10)
+        }
+
+        button4.setOnClickListener {
+            progressBar4.incrementProgressBy(-10)
+        }
+    }
+}
